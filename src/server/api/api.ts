1d673a0b164034fc54d9d2891ad5ceeb80d2e9f0
Filip Chalupa <chalupa.filip@gmail.com>
2023-03-03 18:37:08 +0100

    Add `/api/me` endpoint for token validation

--- src/server/api/api.ts
+++ src/server/api/api.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express, { Router } from 'express';
+import { UserModel } from '../db';
 import { adminController } from './admin';
 
 export const apiController = (config: any): Router => {
@@ -8,11 +9,25 @@ export const apiController = (config: any): Router => {
 
   api.use('/admin', adminController(config));
 
+  api.get('/me', async (req, res) => {
+    const user = await UserModel.findOne({ login: req.auth?.login });
+    if (user === null) {
+      res.status(401);
+      res.json({ error: 'Not authorized' });
+      return;
+    }
+    res.json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatarUrl: user.avatarUrl,
+    });
+  });
+
   api.use((req, res) => {
     res.status(404);
     res.json({
       error: 'Not found',
-      status: 404,
     });
   });
 
