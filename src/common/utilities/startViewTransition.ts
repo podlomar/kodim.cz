842270f329dc89ab3b2bdf5289ff7671dc617be8
Filip Chalupa <chalupa.filip@gmail.com>
2023-03-14 01:29:14 +0100

    View transition: respect reduced motion preference

--- src/common/utilities/startViewTransition.ts
+++ src/common/utilities/startViewTransition.ts
@@ -1,6 +1,6 @@
 export const startViewTransition = async (callback: () => void, finished = () => {}) => {
   if (
-    'startViewTransition' in document && typeof document.startViewTransition === 'function'
+    'startViewTransition' in document && typeof document.startViewTransition === 'function' && window.matchMedia('(prefers-reduced-motion: no-preference)').matches
   ) {
     const transition = document.startViewTransition(callback);
     try {
