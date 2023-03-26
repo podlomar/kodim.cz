daefbc521e8af49cad78020084a32992352a0188
Filip Chalupa <chalupa.filip@gmail.com>
2023-03-26 21:45:58 +0200

    On escape: allow no callback

--- src/common/utilities/useOnEscapeKey.ts
+++ src/common/utilities/useOnEscapeKey.ts
@@ -1,13 +1,17 @@
 import { useEffect } from 'react';
 
-export const useOnEscapeKey = (callback: () => void) => {
+export const useOnEscapeKey = (callback: null | (() => void)) => {
   useEffect(() => {
+    if (callback === null) {
+      return;
+    }
     const listener = (event: KeyboardEvent) => {
       if (event.key === 'Escape') {
         callback();
       }
     };
     window.addEventListener('keydown', listener);
+    // eslint-disable-next-line consistent-return
     return () => {
       window.removeEventListener('keydown', listener);
     };
